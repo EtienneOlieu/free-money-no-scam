e352d38e14f15f29c627341de07c9222e05dfeea
Horrid Entity <etienne.olieu@gmail.com>
2022-03-22 20:46:48 +0100

    Completed assignment

--- src/main/java/com/example/freemoneynoscam/controllers/IndexController.java
+++ src/main/java/com/example/freemoneynoscam/controllers/IndexController.java
@@ -1,6 +1,8 @@
 package com.example.freemoneynoscam.controllers;
 
+import com.example.freemoneynoscam.services.ValidateEmailService;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,18 +11,31 @@ import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class IndexController {
+    private ValidateEmailService ve = new ValidateEmailService();
+
     @GetMapping("/")
     public String index(){
         return "index";
     }
 
+    @GetMapping("/failure")
+    public String failure() {
+        return "failure";
+    }
+
+    @GetMapping("/success")
+    public String success(){
+    return "success";
+    }
+
     @PostMapping("/test")
     public String test(WebRequest dataFromForm){
-        System.out.println(dataFromForm.getParameter("email"));
-        System.out.println(dataFromForm.getParameter("email"));
-        System.out.println(dataFromForm.getParameter("email"));
-        System.out.println(dataFromForm.getParameter("email"));
-        System.out.println(dataFromForm.getParameter("email"));
-        return "redirect:/";
+
+        if (ve.isEmailValid(dataFromForm.getParameter("email"))){
+        return "redirect:/success";
+
+        }
+        return "redirect:/failure";
     }
+
 }
--- src/main/java/com/example/freemoneynoscam/services/ValidateEmailService.java
+++ src/main/java/com/example/freemoneynoscam/services/ValidateEmailService.java
@@ -1,8 +1,20 @@
 package com.example.freemoneynoscam.services;
 
+import com.example.freemoneynoscam.repository.Repository;
+
 public class ValidateEmailService {
+
+    private Repository re = new Repository();
+
     public boolean isEmailValid(String email){
-        //TODO implement logic such that we verify an e-mail is valid
-        return true;
+        if(email.contains("@") && email.contains(".")){
+            re.insertEmailToDB(email);
+            return true;
+        }
+        return false;
+    }
+
+    public void getConnection(){
+        re.connectDB();
     }
 }
